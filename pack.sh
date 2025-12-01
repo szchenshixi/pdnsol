3b4aa4e5cb53c3912c0c343362220c867cf4e096
Shixi CHEN <schenax@connect.ust.hk>
2025-12-01 16:42:13 +0800

    Fix archive name in compression script

--- pack.sh
+++ pack.sh
@@ -9,21 +9,28 @@ stringContain() { case $2 in *$1* ) return 0;; *) return 1;; esac ;}
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 PROJECT_BASE=$(basename ${SCRIPT_DIR})
 PROJECT_DIR=$(dirname ${SCRIPT_DIR})
+echo "SCRIPT_DIR: ${SCRIPT_DIR}"
+echo "PROJECT_BASE: ${PROJECT_BASE}"
+echo "PROJECT_DIR: ${PROJECT_DIR}"
+echo "COMPRESS_THREADS: ${COMPRESS_THREADS}"
+
 if ! stringContain "pdnsol" ${PROJECT_BASE}; then
     echo "Place this script under the project root! Current: ${SCRIPT_DIR}"
     exit -1
 fi
 echo "Changing directory to ${PROJECT_DIR}"
-echo "Compressing ${PROJECT_DIR} to ${ARCHIVE_NAME}.tar.xz"
+echo "Compressing ${PROJECT_DIR}/${PROJECT_BASE} to ${ARCHIVE_NAME}.tar.xz"
 cd ${PROJECT_DIR}
 tar --exclude-vcs --exclude-vcs-ignores                           \
     --exclude="${PROJECT_BASE}/build"                             \
+    --exclude="${PROJECT_BASE}/test/data"                         \
+    --exclude="${PROJECT_BASE}/app.log"                           \
     --exclude="${PROJECT_BASE}/.cache"                            \
     --exclude="${PROJECT_BASE}/.vscode"                           \
     --use-compress-program="xz -9e --threads=${COMPRESS_THREADS}" \
     -cvaf ${PROJECT_BASE}.tar.xz ${PROJECT_BASE}
-if [ $? ] && xz -t ${ARCHIVE_NAME}; then
-    echo "Succeed. Compressed ${PROJECT_DIR} to ${ARCHIVE_NAME}.tar.xz"
+if [ $? -eq 0 ] && xz -t ${PROJECT_BASE}.tar.xz; then
+    echo "Succeed. Compressed ${PROJECT_BASE} to ${PROJECT_BASE}.tar.xz"
 else
     echo "Failed"
     exit -2
